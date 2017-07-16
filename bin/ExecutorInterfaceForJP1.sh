567adacf596e32476ac7b1729e57b1487170b287
kzhr2 <kazuhiro@freelinech.com>
2017-07-16 22:50:58 +0900

    upgrade

--- bin/ExecutorInterfaceForJP1.sh
+++ bin/ExecutorInterfaceForJP1.sh
@@ -1,23 +1,32 @@
 #! /bin/bash
 
-_cScriptDir=$(cd $(dirname $BASH_SOURCE); pwd)
+_cScriptDir=$(cd $(dirname $BASH_SOURCE)/../; pwd)
 
-source ${_cScriptDir}/../conf/EIFJP1.conf
-source ${_cScriptDir}/CoreExecutor.sh ${_cScriptDir}
+_ID=""
+source ${_cScriptDir}/conf/EIFJP1.conf
+source ${_cScriptDir}/bin/CoreExecutor.sh ${_cScriptDir}
 
 function _getShell() {
 
  local cSETTINGSFILEPATH="forJP1"
- local cID=""
- eval cID='$'${IDNAME}
+ local cJOBNAME=""
+ local cWORK_ID=""
+ eval cJOBNAME='$'${IDNAME}
 
- if [[ "${cID}" = "" ]]; then
+ if [[ "${cJOBNAME}" = "" ]]; then
    #statements
    echo "Environment variable [ ${IDNAME} ] is empty. " >&2 #ERRORMESSAGE
    exit 1
  fi
 
- source ${_cScriptDir}/../settings/${cSETTINGSFILEPATH}/IDSettings_${cID}.conf
+ls -l ${_cScriptDir}/settings/${cSETTINGSFILEPATH}/IDMapping.conf
+ cWORK_ID=`grep "${_JOBNAME}" ${_cScriptDir}/settings/${cSETTINGSFILEPATH}/IDMapping.conf || :`
+
+ _ID=${cWORK_ID%%:*}
+
+ source ${_cScriptDir}/settings/${cSETTINGSFILEPATH}/IDSettings_${_ID}.conf
+
+ __Init_LOG_FILE "${_ID}"
 
  __PUTLOG DEBUG "ShellNAME [ ${_SHELLNAME} ]"
  __PUTLOG DEBUG "Options [ ${_OPTIONS} ]"
@@ -25,5 +34,8 @@ function _getShell() {
 }
 
 _getShell
+
+__PUTLOG INFO "Start."
+__PUTLOG INFO "EXECUTE SHELL NAME IS [ ${_SHELLNAME} ]"
 
 __ExecMain ${_SHELLNAME} "${_OPTIONS}"
