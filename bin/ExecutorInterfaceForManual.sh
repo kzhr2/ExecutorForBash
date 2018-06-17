171757ffb59f29e6af51857f88512a168d6b54fe
KT <kazuhiro@freelinech.com>
2018-06-17 23:41:07 +0900

    bugfix

--- bin/ExecutorInterfaceForManual.sh
+++ bin/ExecutorInterfaceForManual.sh
@@ -2,9 +2,9 @@
 
 _cScriptDir=$(cd $(dirname $BASH_SOURCE)/../; pwd)
 
-ARGUMENT_PROGRAM="${1}"
+ARGUMENT_PROGRAM_ID="${1}"
 shift
-ARGUMENT_OPTIONS="${@}"
+ARGUMENT_ARGUMENTS="${@}"
 
 source ${_cScriptDir}/bin/CoreExecutor.sh ${_cScriptDir}
 
@@ -13,25 +13,25 @@ function _getProgram() {
  local cSETTINGSFILEPATH="forManual"
  local cWORK_ID=""
 
- if [[ -e "${_cScriptDir}/module/${ARGUMENT_PROGRAM}" ]]; then
-   #statements
-   echo "Program name is empty. [ ${IDNAME} ]" >&2 #ERRORMESSAGE
-   exit 1
- fi
+ source ${_cScriptDir}/settings/${cSETTINGSFILEPATH}/Configuration_${ARGUMENT_PROGRAM_ID}.conf
 
- source ${_cScriptDir}/settings/${cSETTINGSFILEPATH}/Configuration_${ARGUMENT_PROGRAM}.conf
+  if [[ -e "${_cScriptDir}/module/${_PROGRAM}" ]]; then
+    #statements
+    echo "Program name is empty. [ ${_PROGRAM} ]" >&2 #ERRORMESSAGE
+    exit 1
+  fi
 
  #make LogFile
- __Init_LOG_FILE "${ARGUMENT_PROGRAM}"
+ __Init_LOG_FILE "${ARGUMENT_PROGRAM_ID}"
 
- __PUTLOG DEBUG "PROGRAM [ ${ARGUMENT_PROGRAM} ]"
- __PUTLOG DEBUG "Options [ ${ARGUMENT_OPTIONS} ]"
+ __PUTLOG DEBUG "PROGRAM [ ${_PROGRAM} ]"
+ __PUTLOG DEBUG "Options [ ${ARGUMENT_ARGUMENTS} ]"
 
 }
 
 _getProgram
 
 __PUTLOG INFO "Start."
-__PUTLOG INFO "EXECUTE Program NAME IS [ ${ARGUMENT_PROGRAM} ]"
+__PUTLOG INFO "EXECUTE Program NAME IS [ ${_PROGRAM} ]"
 
-__ExecMain ${ARGUMENT_PROGRAM} "${ARGUMENT_OPTIONS}"
+__ExecMain ${_PROGRAM} "${ARGUMENT_ARGUMENTS}"
